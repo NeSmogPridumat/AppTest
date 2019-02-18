411637458b75b44fb35fe6889bca32f9fc1c1a25
Zaman <zaman-7@mail.ru>
2019-02-18 15:05:09 +0500

    Убрал горизонтальную ориентацию

--- app/src/main/java/com/example/home/mytestapplication/DescriptionActivity.java
+++ app/src/main/java/com/example/home/mytestapplication/DescriptionActivity.java
@@ -1,6 +1,5 @@
 package com.example.home.mytestapplication;
 
-import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
